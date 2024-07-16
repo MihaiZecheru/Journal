988578789ce1436b48084720c78db8b8f3b96c1e
Mihai Zecheru <zecheruchris@gmail.com>
2024-07-16 16:04:53 -0700

    Add landing page

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,9 +1,38 @@
+import { useNavigate } from 'react-router-dom';
+import '../styles/landing.css';
+
 const Landing = () => {
+  const navigate = useNavigate();
+  
+  const onBtnClick = () => {
+    setTimeout(() => navigate('/login'), 250);
+  };
+
   return (
     <div className="landing">
-      TODO
+      <h1 className="animation slide-up no-highlight slower delay-3s">Remember every day with Journal</h1>
+      <div className="logo-animation animation slide-up slower delay-3s no-highlight d-flex align-items-center">
+        <button className="cssbuttons-io-button" onClick={ onBtnClick }>
+          Get started
+          <div className="icon">
+            <svg
+              height="24"
+              width="24"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M0 0h24v24H0z" fill="none"></path>
+              <path
+                d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                fill="currentColor"
+              ></path>
+            </svg>
+          </div>
+        </button>
+        <img src="../journal.png" alt="logo" />
+      </div>
     </div>
   );
-}
- 
+};
+
 export default Landing;
