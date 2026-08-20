4887b05590f90d8b2e796ea534fa75c2656465d1
Mihai Zecheru <zecheruchris@gmail.com>
2026-08-20 14:09:36 -0700

    Add package.json scripts & ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: 'journal-server',
+      script: './server/server.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 3002
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3002
+      }
+    }
+  ]
+};
